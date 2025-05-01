985d21c4b2f68e3de03fb6c4c2ced708ea9f3fcb
João Marcos <101360915+joaomarcosmr@users.noreply.github.com>
2025-05-01 14:45:23 -0300

    api backend

--- src/services/api/apiClient.ts
+++ src/services/api/apiClient.ts
@@ -10,8 +10,9 @@ interface ErrorResponse {
 
 // Base API configuration
 const apiClient: AxiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
-	// 'https://backend-proud-moon-9701.fly.dev/api',
+	baseURL: import.meta.env.VITE_API_URL ||
+		// 'http://localhost:3000/api',
+		'https://backend-proud-moon-9701.fly.dev/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
