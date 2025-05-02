ee0c5d9bf303e6c2b8c2964bbcf4ae2e4a657c6c
João Marcos <101360915+joaomarcosmr@users.noreply.github.com>
2025-05-01 22:32:36 -0300

    subindo permissions arrumado

--- src/pages/admin/Permissions.tsx
+++ src/pages/admin/Permissions.tsx
@@ -8,7 +8,7 @@ import {
 	CardHeader,
 	CardTitle
 } from '@/components/ui/card';
-import { User, Shield, Edit, Trash2, Plus, Save, X, AlertCircle, UserPlus } from 'lucide-react';
+import { User, Shield, Edit, Trash2, Plus, Save, X, AlertCircle, UserPlus, Loader2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
@@ -27,13 +27,15 @@ import { Badge } from '@/components/ui/badge';
 import { toast } from 'sonner';
 import { useAuth } from '@/context/AuthContext';
 import { Alert, AlertDescription } from '@/components/ui/alert';
-import UserService, { RoleEnum, User as UserType } from '@/services/api/UserService';
+import UserService, { RoleEnum, UserPermissions, User as UserType } from '@/services/api/UserService';
 import { allPermissions, DEFAULT_PERMISSIONS } from '@/utils/permissions';
 
 // Componente principal
 const Permissions = () => {
 	const { companySelected } = useAuth();
 	const [users, setUsers] = useState<UserType[]>([]);
+	const [isDeleting, setIsDeleting] = useState<number | null>(null);
+	const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState<UserType | null>(null);
 	const [searchTerm, setSearchTerm] = useState('');
 	const { user: currentUser } = useAuth();
 	const [selectedUser, setSelectedUser] = useState<UserType | null>(null);
@@ -44,6 +46,7 @@ const Permissions = () => {
 		role: RoleEnum.USER,
 		companyId: companySelected.id
 	});
+	const [selectedPermissions, setSelectedPermissions] = useState<UserPermissions>();
 	const [isUserModalOpen, setIsUserModalOpen] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState<string | null>(null);
@@ -67,13 +70,69 @@ const Permissions = () => {
 		}
 	};
 
+	const fetchUserPermission = async (userId: number) => {
+		try {
+			setIsLoading(true);
+			const response = await UserService.getPermissions(userId);
+
+			if (response.success && response.data) {
+				const permissions: UserPermissions = {
+					userId,
+					...response.data, // assume que o response.data contém apenas os campos booleanos
+				};
+
+				setSelectedPermissions(permissions);
+			}
+		} catch (error) {
+			toast.error('Erro ao carregar permissões do usuário');
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const handleSavePermissions = async () => {
+		if (!selectedPermissions) return;
+
+		try {
+			const response = await UserService.updatePermissions(selectedPermissions.userId, selectedPermissions);
+			const responseUser = await UserService.update(selectedUser.id, selectedUser);
+
+			if (response.success && responseUser.success) {
+				setSelectedPermissions(null)
+				setSelectedUser(null);
+
+				await fetchUsersData();
+				toast.success('Permissões salvas com sucesso!');
+			}
+		} catch (error) {
+			toast.error('Erro inesperado ao salvar permissões.');
+		}
+	};
+
 	// Edição de usuário
 	const handleEditUser = (user: UserType) => {
 		setSelectedUser(user);
+		fetchUserPermission(user.id)
 	};
 
-	const handleDeleteUser = (userId: number) => {
+	const handleDeleteUser = async (userId: number) => {
+		setIsDeleting(userId);
+		try {
+			const response = await UserService.delete(userId);
 
+			if (response.success) {
+				toast.success('Serviço excluído com sucesso');
+				fetchUsersData();
+				setIsDeleteDialogOpen(null);
+				setSelectedUser(null);
+			}
+		} catch (error) {
+			toast.error('Erro ao conectar com o servidor');
+			console.error(error);
+		} finally {
+			setIsDeleteDialogOpen(null)
+			setIsDeleting(null);
+		}
 	};
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -86,6 +145,15 @@ const Permissions = () => {
 		setNewUser(prev => ({
 			...prev,
 			role: value === 'MANAGER' ? RoleEnum.MANAGER : RoleEnum.USER
+		}));
+	};
+
+	const handleToggle = (id: keyof UserPermissions) => {
+		if (!selectedPermissions) return;
+
+		setSelectedPermissions((prev) => ({
+			...prev!,
+			[id]: !prev![id],
 		}));
 	};
 
@@ -311,14 +379,14 @@ const Permissions = () => {
 													className={
 														user.role === RoleEnum.ADMIN
 															? 'bg-purple-100 text-purple-800 hover:bg-purple-200'
-															: user.role === RoleEnum.MANAGER
+															: user.role === RoleEnum.USER
 																? 'bg-blue-100 text-blue-800 hover:bg-blue-200'
 																: 'bg-green-100 text-green-800 hover:bg-green-200'
 													}
 												>
 													{user.role === RoleEnum.ADMIN
 														? 'Administrador'
-														: user.role === RoleEnum.MANAGER
+														: user.role === RoleEnum.USER
 															? 'Funcionário'
 															: 'Recepcionista'}
 												</Badge>
@@ -338,7 +406,9 @@ const Permissions = () => {
 														variant="outline"
 														size="sm"
 														className="h-8 px-2 text-red-500 hover:text-red-600 border-red-200 hover:border-red-300"
-														onClick={() => handleDeleteUser(user.id)}
+														onClick={() => {
+															setIsDeleteDialogOpen(user)
+														}}
 														disabled={user.id === currentUser?.id}
 													>
 														<Trash2 className="h-4 w-4" />
@@ -407,15 +477,17 @@ const Permissions = () => {
 									<Label htmlFor="role">Função</Label>
 									<Select
 										value={selectedUser.role}
-										onValueChange={(value: RoleEnum) => setSelectedUser({ ...selectedUser, role: value })}
+										onValueChange={(value: string) =>
+											setSelectedUser({ ...selectedUser, role: value as RoleEnum })
+										}
 									>
 										<SelectTrigger className="mt-1">
 											<SelectValue placeholder="Selecione uma função" />
 										</SelectTrigger>
 										<SelectContent>
-											<SelectItem value="admin">Administrador</SelectItem>
-											<SelectItem value="barber">Barbeiro</SelectItem>
-											<SelectItem value="receptionist">Recepcionista</SelectItem>
+											<SelectItem value={RoleEnum.ADMIN}>Administrador</SelectItem>
+											<SelectItem value={RoleEnum.MANAGER}>Recepcionista</SelectItem>
+											<SelectItem value={RoleEnum.USER}>Funcionário</SelectItem>
 										</SelectContent>
 									</Select>
 								</div>
@@ -427,10 +499,11 @@ const Permissions = () => {
 											<div key={permission.id} className="flex items-start space-x-3 py-2">
 												<Checkbox
 													id={`perm-${permission.id}`}
-													// Desabilitar checkbox se for admin (admin tem todas as permissões)
-													disabled={selectedUser.role === 'ADMIN'}
-													// Checked se for admin ou se a permissão estiver na lista de permissões do papel
-													checked={selectedUser.role === 'ADMIN' || true}
+													disabled={selectedUser.role === RoleEnum.ADMIN}
+													checked={
+														selectedUser.role === RoleEnum.ADMIN || selectedPermissions?.[permission.id] === true
+													}
+													onCheckedChange={() => handleToggle(permission.id as keyof UserPermissions)}
 												/>
 												<div className="space-y-1">
 													<Label
@@ -449,11 +522,38 @@ const Permissions = () => {
 
 							<DialogFooter>
 								<Button variant="outline" onClick={() => setSelectedUser(null)}>Cancelar</Button>
-								<Button onClick={() => handleEditUser(selectedUser)}>Salvar Alterações</Button>
+								<Button onClick={() => handleSavePermissions()}>Salvar Alterações</Button>
 							</DialogFooter>
 						</DialogContent>
 					</Dialog>
 				)}
+
+				{/* Delete Service Dialog */}
+				<Dialog open={!!isDeleteDialogOpen} onOpenChange={(open) => !open && setIsDeleteDialogOpen(null)}>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Confirmar Exclusão</DialogTitle>
+							<DialogDescription>
+								Tem certeza que deseja excluir o serviço "{isDeleteDialogOpen?.name}"?
+								Esta ação não pode ser desfeita.
+							</DialogDescription>
+						</DialogHeader>
+						<DialogFooter>
+							<Button variant="outline" onClick={() => setIsDeleteDialogOpen(null)} disabled={isDeleting !== null}>
+								Cancelar
+							</Button>
+							<Button variant="destructive" onClick={() => isDeleteDialogOpen?.id && handleDeleteUser(isDeleteDialogOpen.id)} disabled={isDeleting !== null}>
+								{isDeleting !== null ? (
+									<>
+										<Loader2 className="mr-2 h-4 w-4 animate-spin" />
+										Excluindo...
+									</>
+								) : 'Excluir'}
+							</Button>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
+
 			</div>
 		</AdminLayout>
 	);
